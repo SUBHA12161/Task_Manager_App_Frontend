5e50ab826aa2699aaa3332cdd60c51814cb6cffe
Subharanjan Rout <crout12161@gmail.com>
2024-10-19 11:52:23 +0530

    nav bar correction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const MyNavbar = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand as={NavLink} to="/" exact>Task Manager</Navbar.Brand>
+                <Navbar.Brand as={NavLink} to="/home" exact>Task Manager</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
